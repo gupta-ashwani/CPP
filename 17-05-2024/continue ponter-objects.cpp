91bc1f503795ba84c7a14b971a90a3215a1e9112
Ashwani Gupta <161638810+gupta-ashwani@users.noreply.github.com>
2024-05-17 11:33:05 +0530

    continue ponter-objects.cpp

---
+++ 17-05-2024/continue ponter-objects.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class demo
+{
+    int i;
+
+public:
+    demo(int x)
+    {
+        i = x;
+    }
+    int getdata()
+    {
+        return i;
+    }
+};
+
+int main()
+{
+    demo d(55), *ptr;
+    ptr = &d;
+    cout << ptr->getdata();
+    return 0;
+}
